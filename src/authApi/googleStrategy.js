13d18cb588b3c5350480e235c20541ed3a40ca5f
root <root@srv961357.hstgr.cloud>
2025-08-19 15:14:10 +0000

    hello

--- src/authApi/googleStrategy.js
+++ src/authApi/googleStrategy.js
@@ -6,7 +6,7 @@ const db = require('../../config/db');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:1000/api/auth/google/callback"
+  callbackURL: "https://backedbyquantum.com/home"
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
